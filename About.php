d697c9c1961d1fdc5f84c00900ccb598f1f676aa
NaiArt <artthanavit@gmail.com>
2017-04-21 20:12:32 +0700

    Add files via upload

---
+++ About.php
@@ -0,0 +1,147 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<!-- Custom Theme files -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<!-- Custom Theme files -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--webfont-->
+<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+<script type="text/javascript" src="js/hover_pack.js"></script>
+<!-- start menu -->
+<link href="css/megamenu.css" rel="stylesheet" type="text/css" media="all" />
+<script type="text/javascript" src="js/megamenu.js"></script>
+<script>$(document).ready(function(){$(".megamenu").megamenu();});</script>
+<script type="text/javascript">
+					jQuery(document).ready(function($) {
+						$(".scroll").click(function(event){		
+							event.preventDefault();
+							$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+						});
+					});
+</script>
+</head>
+<body>
+	<div class="banner">
+   	  <div class="container">
+   		<div class="logo">
+			<a href="main.php"><img src="images/logo.png" alt=""/></a>
+		</div>
+	   <div class="menu">
+	     <ul class="megamenu skyblue">
+			</ul>
+			</div>
+	        <div class="clearfix"> </div>
+	        <div class="header_arrow">
+	          <a href="#arrow" class="scroll"><span> </span></a>
+	        </div>
+       </div>
+   </div>
+<section id="doctor-team" class="section-padding">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<br><h2 class="ser-title">Meet Our Adim!</h2>
+					<hr class="botm-line">
+				</div>
+				<div class="col-md-3 col-sm-3 col-xs-6">
+			      <div class="thumbnail"> 
+			      	<img src="img/phaka.jpg" alt="..." class="team-img">
+			        <div class="caption">
+			          <h3>Phaka Thammakirati</h3>
+			          <p>Adim</p>
+			          <ul class="list-inline">
+			            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+			            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+			            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+			          </ul>
+			        </div>
+			      </div>
+			    </div>
+			    <div class="col-md-3 col-sm-3 col-xs-6">
+			      <div class="thumbnail"> 
+			      	<img src="img/thanavit.jpg" alt="..." class="team-img">
+			        <div class="caption">
+			          <h3>Thanavit Arjhan</h3>
+			          <p>Adim</p>
+			          <ul class="list-inline">
+			            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+			            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+			            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+			          </ul>
+			        </div>
+			      </div>
+			    </div>
+			    <div class="col-md-3 col-sm-3 col-xs-6">
+			      <div class="thumbnail"> 
+			      	<img src="img/miao.jpg" alt="..." class="team-img">
+			        <div class="caption">
+			          <h3>Miao Baoyu</h3>
+			          <p>Adim</p>
+			          <ul class="list-inline">
+			            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+			            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+			            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+			          </ul>
+			        </div>
+			      </div>
+			    </div>
+			    <div class="col-md-3 col-sm-3 col-xs-6">
+			      <div class="thumbnail"> 
+			      	<img src="img/dittawat.jpg" alt="..." class="team-img">
+			        <div class="caption">
+			          <h3>Dittawat Kaewseehabut</h3>
+			          <p>Adim</p>
+			          <ul class="list-inline">
+			            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+			            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+			            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+			          </ul>
+			        </div>
+			      </div>
+			    </div>
+			    <div class="col-md-3 col-sm-3 col-xs-6">
+			      <div class="thumbnail"> 
+			      	<img src="img/thanet.jpg" alt="..." class="team-img">
+			        <div class="caption">
+			          <h3>Thanet Nakarat</h3>
+			          <p>Servant</p>
+			          <ul class="list-inline">
+			            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+			            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+			            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+			          </ul>
+			        </div>
+			      </div>
+			    </div>
+			</div>
+		</div>
+	</section>
+	   <div class="footer">
+   	 <div class="container">
+   		<div class="cssmenu">
+				<ul>
+					<li class="active"><a href="#">Privacy</a></li>
+					<li><a href="#">Terms</a></li>
+					<li><a href="#">Shop</a></li>
+					<li><a href="About.php">About</a></li>
+					<li><a href="contact.html">Contact</a></li>
+				</ul>
+		</div>
+		<ul class="social">
+			<li><a href=""> <i class="instagram"> </i> </a></li>
+			<li><a href=""><i class="fb"> </i> </a></li>
+			<li><a href=""><i class="tw"> </i> </a></li>
+	    </ul>
+	    <div class="clearfix"></div>
+	    <div class="copy">
+           <p>&copy; Template by <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>
+	    </div>
+   	</div>
+   </div>
+	</body>
+</html>	
