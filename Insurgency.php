82c57ba94236906b62e44729f27b41ad4571e35a
ThanetNakarat <yoyo_ran_ter@hotmail.com>
2017-04-27 14:30:43 +0700

    Add files via upload

---
+++ Insurgency.php
@@ -0,0 +1,277 @@
+﻿<!DOCTYPE HTML>
+<html>
+<head>
+<title>White Noise 2</title>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<!-- Custom Theme files -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href="css/component.css" rel='stylesheet' type='text/css' />
+<!-- Custom Theme files -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--webfont-->
+<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+<!-- start menu -->
+<link href="css/megamenu.css" rel="stylesheet" type="text/css" media="all" />
+<script type="text/javascript" src="js/megamenu.js"></script>
+<script>$(document).ready(function(){$(".megamenu").megamenu();});</script>
+<link rel="stylesheet" href="css/etalage.css">
+<script src="js/jquery.etalage.min.js"></script>
+<script>
+			jQuery(document).ready(function($){
+
+				$('#etalage').etalage({
+					thumb_image_width: 300,
+					thumb_image_height: 400,
+					source_image_width: 900,
+					source_image_height: 1200,
+					show_hint: true,
+					click_callback: function(image_anchor, instance_id){
+						alert('Callback example:\nYou clicked on an image with the anchor: "'+image_anchor+'"\n(in Etalage instance: "'+instance_id+'")');
+					}
+				});
+
+			});
+		</script>
+<script src="js/easyResponsiveTabs.js" type="text/javascript"></script>
+		    <script type="text/javascript">
+			    $(document).ready(function () {
+			        $('#horizontalTab').easyResponsiveTabs({
+			            type: 'default', //Types: default, vertical, accordion           
+			            width: 'auto', //auto or any width like 600px
+			            fit: true   // 100% fit in a container
+			        });
+			    });
+            </script>	
+</head>
+<body>
+
+	<div class="men_banner">
+   	  <div class="container">
+   		<div class="logo">
+			<a href="main.php"><img src="images/ss.png" alt=""/></a>
+		</div>
+	  
+  <div class="menu">
+	     <ul class="megamenu skyblue">
+			<li><a class="color2" href="shop.html">Shop</a>
+		<div class="megapanel">
+					<div class="row">
+						<div class="col1">
+							<div class="h_nav">
+								<ul>
+									<li><a href="men.html">Action</a></li>
+									<li><a href="men.html">Adventure</a></li>
+									<li><a href="men.html">Cascual</a></li>
+									<li><a href="men.html">Sports</a></li>
+									<li><a href="men.html">RPG</a></li>
+									<li><a href="men.html">Simulation</a></li>
+								</ul>	
+							</div>							
+						</div>
+				<li><a class="color7" href="404.html">News</a></li>
+				<div class="clearfix"> </div>
+			</ul>
+			</div>
+	        <div class="clearfix"> </div>
+	    </div>
+   </div>
+   <div class="men">
+   		
+   	<div class="container">
+      <div class="col-md-9 single_top">
+	  <div class="dreamcrub">
+			   	<ul class="breadcrumbs">
+                    <li class="home">
+                       <a href="shop.php" title="Go to Home Page">Shop</a>&nbsp;
+                       <span>&gt;</span>
+                    </li>
+                    <li class="home">&nbsp;
+                       ALL GAME&nbsp;
+                    </li>
+                </ul>
+             
+                <div class="clearfix"></div>
+			   </div>
+      	<div class="single_left">
+      	  <div class="labout span_1_of_a1">
+			<ul id="etalage">
+			
+							<li>
+								<a href="optionallink.html">
+									<img class="etalage_thumb_image" src="img/kill.jpg" class="img-responsive" />
+									<img class="etalage_source_image" src="img/kill2.jpg" class="img-responsive" title="" />
+								</a>
+							</li>
+							<li>
+								<img class="etalage_thumb_image" src="img/kill3.jpg" class="img-responsive" />
+								<img class="etalage_source_image" src="img/kill4.jpg"" class="img-responsive" title="" />
+							</li>
+						
+							
+			 </ul>
+		     <div class="clearfix"></div>	
+	    </div>
+		
+		<div class="cont1 span_2_of_a1">
+				<h1>Batman™: Arkham Knight</h1>
+				<p class="availability">Availability: <span class="color">In stock</span></p>
+			    <div class="price_single">
+				  
+				  <span class="actual">$29.99</span><a href="#">click for offer</a>
+				</div>
+				<h2 class="quick">About :</h2>
+				<p class="quick_desc"> Be a part of the investigator team, or take control of the creature and devour them! White Noise 2 offers a 4vs1 asymmetric horror experience that won't leave anyone unmoved. </p>
+			    <div class="wish-list">
+				 	<ul>
+				 	    <li class="compare"><a href="http://cdn.edgecast.steamstatic.com/steam/apps/2028792/movie480.webm?t=1447358063">Gameplay</a></li>
+				 	</ul>
+				 </div>
+				
+				<div class="quantity_box">
+					<ul class="product-qty">
+					   <span>Quantity:</span>
+					   <select>
+						 <option>1</option>
+						 <option>2</option>
+						 <option>3</option>
+						 <option>4</option>
+						 <option>5</option>
+						 <option>6</option>
+					   </select>
+				    </ul>
+				    <ul class="single_social">
+						<li><a href="#"><i class="fb1"> </i> </a></li>
+						<li><a href="#"><i class="tw1"> </i> </a></li>
+						<li><a href="#"><i class="g1"> </i> </a></li>
+					
+		   		    </ul>
+		   		    <div class="clearfix"></div>
+	   		    </div>
+			    <a href="reservation.html" title="Online Reservation" class="btn btn-primary btn-normal btn-inline " target="_self">Buy</a>
+			</div>
+		    <div class="clearfix"> </div>
+		</div>
+        <div class="sap_tabs">	
+				     <div id="horizontalTab" style="display: block; width: 100%; margin: 0px;">
+						  <ul class="resp-tabs-list">
+						  	  <li class="resp-tab-item" aria-controls="tab_item-0" role="tab"><span>Product Description</span></li>
+							  <li class="resp-tab-item" aria-controls="tab_item-1" role="tab"><span>SYSTEM REQUIREMENTS</span></li>
+							  <li class="resp-tab-item" aria-controls="tab_item-2" role="tab"><span>Reviews</span></li>
+							  <div class="clear"></div>
+						  </ul>				  	 
+							<div class="resp-tabs-container">
+							    <div class="tab-1 resp-tab-content" aria-labelledby="tab_item-0">
+									<div class="facts">
+									  <ul class="tab_list">
+									
+									  <ul>
+										<li>Years after taking Stilwater for their own, the Third Street Saints have evolved from street gang to household brand name, with Saints sneakers, Saints energy drinks and Johnny Gat bobble head dolls all available at a store near you.</li><li>The Saints are kings of Stilwater, but their celebrity status has not gone unnoticed. The Syndicate, a legendary criminal fraternity with pawns in play all over the globe, has turned its eye on the Saints and demands tribute. Refusing to kneel to the Syndicate, you take the fight to Steelport, a once-proud metropolis reduced to a struggling city of sin under Syndicate control. </li>
+									          
+							        </div>
+							     </div>	
+							      <div class="tab-1 resp-tab-content" aria-labelledby="tab_item-1">
+									<div class="facts">
+									  <ul class="tab_list">
+									  								<table>
+									<tr>
+									  	 <th>MINIMUM:</th>
+    <th>RECOMMENDED:</th>
+  </tr>
+  <tr><td>  Windows XP SP3 <br>
+ Processor: Dual Core processor <br>
+Memory: 2 GB RAM<br>
+Graphics: DirectX 10 capable hardware <br>
+Storage: 1 GB available space<br>
+Sound Card: DX 8.1 Compatible Audio <br></td>
+    <td>  Windows 7 or newer  <br>
+Processor: Quad core processor <br>
+Memory: 2 GB RAM<br>
+  GeForce 660 or better <br> 
+Storage: 2 GB available space<br>
+Sound Card: Eax Compatible <br></td>
+  </tr>
+</table>
+</ul>
+							        </div>
+							     </div>	
+							      <div class="tab-1 resp-tab-content" aria-labelledby="tab_item-2">
+									<ul class="tab_list">
+	
+									  </ul>      
+							     </div>	
+							 </div>
+					      </div>
+					 </div>	
+		</div>
+		<div class="col-md-3">
+	      <h3 class="m_1">Related Products</h3>
+	      <ul class="product">
+	      	<li class="product_img"><img src="images/saint.jpg" class="img-responsive" alt=""/></li>
+	      	<li class="product_desc">
+	      		<h4><a href="#">SaintRow4</a></h4>
+	      		<p class="single_price">$30</p>
+	      		<a href="#" class="link-cart">Add to Wishlist</a>
+	      	    
+	        </li>
+	      	<div class="clearfix"> </div>
+	      </ul>
+	      <ul class="product">
+	      	<li class="product_img"><img src="images/H1.jpg" class="img-responsive" alt=""/></li>
+	      	<li class="product_desc">
+	      		<h4><a href="#">H1Z1</a></h4>
+	      		<p class="single_price">$18</p>
+	      		<a href="#" class="link-cart">Add to Wishlist</a>
+	      	    
+	        </li>
+	      	<div class="clearfix"> </div>
+	      </ul>
+	      <ul class="product">
+	      	<li class="product_img"><img src="images/how.png" class="img-responsive" alt=""/></li>
+	      	<li class="product_desc">
+	      		<h4><a href="how.php">How to Survive</a></h4>
+	      		<p class="single_price">$15.99</p>
+	      		<a href="#" class="link-cart">Add to Wishlist</a>
+	      	    
+	        </li>
+	      	<div class="clearfix"> </div>
+	      </ul>
+	     <ul class="product">
+	      	<li class="product_img"><img src="ac/ac.jpg" class="img-responsive" alt=""/></li>
+	      	<li class="product_desc">
+	      		<h4><a href="ac.php">Assassin’s Creed® IV Black Flag™</a></h4>
+	      		<p class="single_price">$29.99</p>
+	      		<a href="#" class="link-cart">Add to Wishlist</a>
+	        </li>
+	      	<div class="clearfix"> </div>
+	      </ul>
+	      	<div class="clearfix"> </div>
+	      </ul>
+        </div>
+     <div class="clearfix"> </div>
+	</div>
+    </div>
+   </div>
+   <div class="footer">
+   	 <div class="container">
+   		<div class="cssmenu">
+				<ul>
+					<li class="active"><a href="#">Privacy</a></li>
+					<li><a href="#">Terms</a></li>
+					<li><a href="#">Shop</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="contact.html">Contact</a></li>
+				</ul>
+		</div>
+		<ul class="social">
+			<li><a href=""> <i class="instagram"> </i> </a></li>
+			<li><a href=""><i class="fb"> </i> </a></li>
+			<li><a href=""><i class="tw"> </i> </a></li>
+	    </ul>
+	    <div class="clearfix"> </div>
+   	</div>
+   </div>
+</body>
+</html>		
